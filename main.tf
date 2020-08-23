c89479b03a4a99ff35ed2a06949273e6f08107e9
zack-klein <klein.zachary.j@gmail.com>
2020-08-23 09:25:02 -0700

    fix: Update path to policy.json

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "b" {
   bucket = var.bucket_name
   policy = templatefile(
-    "./modules/s3-website/policy.json",
+    "./policy.json",
     {
       bucket                            = var.bucket_name
       cloudfront_origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn
