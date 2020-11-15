f18e14826571b552f402e4a58a49530e383b54ab
zack-klein <klein.zachary.j@gmail.com>
2020-11-14 19:38:35 -0500

    fix: Add fix for react router

--- cloudfront.tf
+++ cloudfront.tf
@@ -37,4 +37,13 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
       restriction_type = "none"
     }
   }
+
+  custom_error_response = [
+    {
+      error_caching_min_ttl = 5
+      error_code = 403
+      response_code = 200
+      response_page_path = "/index.html"
+    }
+  ]
 }
